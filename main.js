4c0afa0743c40f8c268d4a21fcaaa07ac1dc86ee
Julien Galibois Sauvageau <juliengs@hotmail.com>
2018-10-03 22:53:20 -0400

    Early resource gathering

--- main.js
+++ main.js
@@ -4,16 +4,55 @@ var wood = 0;
 var lumberjacks = 0;
 var stone = 0;
 var miners = 0;
+var exploredArea = 0;
+
+function scout(){
+    exploredArea++;
+    console.log('Explored area is ' + exploredArea);
+    document.getElementById('explored').innerHTML = exploredArea; 
+    unveilStuff(exploredArea); 
+};
+
+function unveilStuff(exploredArea){
+    if (exploredArea == 1){
+        $('.right-tab').append("<p class='log-text'>You see you are not alone. Friend?</p>");
+        $('#people-resource').removeClass("hidden");
+    }
+    else if (exploredArea == 2){
+        $('.right-tab').append("<p class='log-text'>You look around some more....</p>");
+    }
+    else if (exploredArea == 3){
+        $('.right-tab').append("<p class='log-text'>You found a few bushes. They seem to have berries on them.</p>");
+        $('#forage-food-btn').removeClass("hidden");
+        $('#scout-btn').text('Scout area');
+    }
+    else if (exploredArea == 5){
+        $('.right-tab').append("<p class='log-text'>You found some branches on the ground. You should gather a few.</p>");
+        $('#gather-wood-btn').removeClass("hidden");
+    }
+    else if (exploredArea == 7){
+        $('.right-tab').append("<p class='log-text'>You found some stones, they should be useful.</p>");
+        $('#gather-stone-btn').removeClass("hidden");
+    }
+};
+    
+    
 
 function getFood(number){
+    if(food > 0)
+        {$('#food-resource').removeClass("hidden");}
     food = food + number;
     document.getElementById("food").innerHTML = food;
 }
 function getWood(number){
+    if(wood > 0)
+        {$('#wood-resource').removeClass("hidden");}
     wood = wood + number;
     document.getElementById("wood").innerHTML = wood;
 }
 function getStone(number){
+    if(stone > 0)
+        {$('#stone-resource').removeClass("hidden");}
     stone = stone + number;
     document.getElementById("stone").innerHTML = stone;
 }
@@ -57,10 +96,6 @@ function hireMiner(){
 
 //Makes tabs resizable
 $(".left-tab, .middle-tab, .right-tab").resizable({containment: ".main"});
-
-
-
-
 $('.left-tab').resize(function(){
    $('.middle-tab').width($(".main").width()-$(".left-tab").width()); 
 });
@@ -70,6 +105,11 @@ $(window).resize(function(){
 });
 
 
+
+
+
+//Sets the current age
+$('#era').text('Ancient Era')
 
 // Game loop
 // Do not put anything below this line
