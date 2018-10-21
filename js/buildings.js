a7baf38630689260505ce48dfb66b97990e4fdb3
Julien Galibois Sauvageau <juliengs@hotmail.com>
2018-10-21 15:55:33 -0400

    JS files organization

--- js/buildings.js
+++ js/buildings.js
@@ -1,3 +1,173 @@
+
+
+
+
+//Common function to build all buildings
+//See comments to see what it does
+
+function buildBuilding(input) {
+    hide('#build-button',100);
+    //Checks if the building is already built
+    if (input.built == 1) {
+        logText(input.name + ' already built!');
+        return;
+    }
+    //First loop checks how many resource types are needed to build the building
+    var valid = 0;
+    var passed = 0;
+    for (i = 1; i < 6; i++) {
+        a = eval('input.costs_' + i);
+        if (a !== undefined) {
+            valid++;
+        }
+    }
+    //Second loop runs as many times as there are resource types used to build the building
+
+    for (j = 1; j <= valid; j++) {
+
+        var amtCost = eval('input.costs_' + j + '_amount'); // Amount of resources required
+        var nmCost = eval('input.costs_' + j); // Name of resource required
+        var nmInvLower = nmCost.toLowerCase(); // Name of resource, lower case to match the inventory resource type
+        var amtInv = eval('resource.' + nmInvLower); // Amount of resource in inventory
+        if (amtInv >= amtCost) {
+            passed++;
+        }
+        else {
+            //Not enough resources
+            logText('Not enough ' + nmCost + '!')
+        }
+    }
+    if (passed == valid) {
+
+        //Resource check successful, proceed
+
+        //Spends the required resources
+        for (k = 1; k <= valid; k++) {
+            var amtCost2 = eval('input.costs_' + k + '_amount'); // Amount of resources required
+            var nmCost2 = eval('input.costs_' + k); // Name of resource required
+            var nmInvLower2 = nmCost2.toLowerCase(); // Name of resource, lower case to match the inventory resource type
+            resource[nmInvLower2] -= amtCost2;
+        }
+
+        //Marks the building as built
+        buildings[input.number].built = 1;
+        $('#left-tab-cell-' + input.number).addClass('built');
+
+        //Displays the related messages
+        logText(input.message);
+
+        //Unlocks the related buildings
+        if (buildings[input.number].unlocks_buildings !== undefined){
+        var unlockedBuildings = buildings[input.number].unlocks_buildings; //array containing list of buildings unlocked
+        for (l = 0; l < unlockedBuildings.length; l++) {
+            buildings[unlockedBuildings[l]].displayable = 1;
+            }
+        }
+
+        //Updates UI for new buildings
+        displayBuildings();
+
+        //Runs the corresponding function specific to this building
+        buildings[input.number].buildFunction();
+    }
+}
+
+
+function showCampfire(){
+    showTooltip(campfire);
+};
+
+function showShamanHut(){
+    showTooltip(shaman_hut);
+};
+
+function showHunterPost(){
+    showTooltip(hunter_post);
+};
+
+function showVillageCenter(){
+    showTooltip(village_center);
+};
+function showWoodChopper(){
+    showTooltip(woodchopper_post);
+};
+function showMinerHut(){
+    showTooltip(miner_hut);
+};
+function showWheatFarm(){
+    showTooltip(wheat_farm);
+}
+
+
+
+
+function buildCampfire(){
+    storage.food =+ 10;
+    storage.wood =+ 10;
+    storage.stone =+ 10;
+    
+};
+
+function buildShamanHut(){
+    show('#research-title',100);
+    hide('#build-button', 100);
+};
+
+function buildHunterPost(){
+};
+
+function buildVillageCenter(){
+    show('#manage-tribe-btn',100);
+    document.getElementById('manage-tribe-btn').addEventListener("click", manageTribe);
+};
+
+function buildWoodChopper(){
+};
+
+function buildMinerHut(){
+};
+
+function buildWheatFarm(){
+};
+
+//Auto-resource workers purchasing
+/*function hireForager() {
+    var workerCost = Math.floor(10 * Math.pow(1.1, foragers));
+    if (resource.food >= workerCost) {
+        foragers = foragers + 1;
+        resource.food = resource.food - workerCost;
+        document.getElementById('foragers').innerHTML = foragers;
+        document.getElementById('food').innerHTML = resource.food;
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1, foragers));
+    document.getElementById('foragerCost').innerHTML = nextCost;
+};
+
+function hireLumberjack() {
+    var workerCost = Math.floor(10 * Math.pow(1.1, lumberjacks));          //works out the cost of this lumberjack
+    if (resource.wood >= workerCost) {                                              //checks that the player can afford the lumberjack
+        lumberjacks = lumberjacks + 1;                                     //increases number of lumberjacks
+        resource.wood = resource.wood - workerCost;                                          //removes the wood spent
+        document.getElementById('lumberjacks').innerHTML = lumberjacks;    //updates the number of lumberjacks for the user
+        document.getElementById('wood').innerHTML = resource.wood;                  //updates the number of wood for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1, lumberjacks));            //works out the cost of the next lumberjack
+    document.getElementById('lumberjackCost').innerHTML = nextCost;        //updates the lumberjack cost for the user
+};
+
+function hireMiner() {
+    var workerCost = Math.floor(10 * Math.pow(1.1, miners));
+    if (resource.stone >= workerCost) {
+        miners = miners + 1;
+        resource.stone = resource.stone - workerCost;
+        document.getElementById('miners').innerHTML = miners;
+        document.getElementById('stone').innerHTML = resource.stone;
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1, miners));
+    document.getElementById('minerCost').innerHTML = nextCost;
+};*/
+
+
 //Buildings
 var buildings = [
     campfire = {
@@ -183,170 +353,3 @@ var buildings = [
         unlocks_buildings: undefined
     },
 ]
-
-
-
-//Common function to build all buildings
-//See comments to see what it does
-
-function buildBuilding(input) {
-    hide('#build-button',100);
-    //Checks if the building is already built
-    if (input.built == 1) {
-        logText(input.name + ' already built!');
-        return;
-    }
-    //First loop checks how many resource types are needed to build the building
-    var valid = 0;
-    var passed = 0;
-    for (i = 1; i < 6; i++) {
-        a = eval('input.costs_' + i);
-        if (a !== undefined) {
-            valid++;
-        }
-    }
-    //Second loop runs as many times as there are resource types used to build the building
-
-    for (j = 1; j <= valid; j++) {
-
-        var amtCost = eval('input.costs_' + j + '_amount'); // Amount of resources required
-        var nmCost = eval('input.costs_' + j); // Name of resource required
-        var nmInvLower = nmCost.toLowerCase(); // Name of resource, lower case to match the inventory resource type
-        var amtInv = eval('resource.' + nmInvLower); // Amount of resource in inventory
-        if (amtInv >= amtCost) {
-            passed++;
-        }
-        else {
-            //Not enough resources
-            logText('Not enough ' + nmCost + '!')
-        }
-    }
-    if (passed == valid) {
-
-        //Resource check successful, proceed
-
-        //Spends the required resources
-        for (k = 1; k <= valid; k++) {
-            var amtCost2 = eval('input.costs_' + k + '_amount'); // Amount of resources required
-            var nmCost2 = eval('input.costs_' + k); // Name of resource required
-            var nmInvLower2 = nmCost2.toLowerCase(); // Name of resource, lower case to match the inventory resource type
-            resource[nmInvLower2] -= amtCost2;
-        }
-
-        //Marks the building as built
-        buildings[input.number].built = 1;
-        $('#left-tab-cell-' + input.number).addClass('built');
-
-        //Displays the related messages
-        logText(input.message);
-
-        //Unlocks the related buildings
-        if (buildings[input.number].unlocks_buildings !== undefined){
-        var unlockedBuildings = buildings[input.number].unlocks_buildings; //array containing list of buildings unlocked
-        for (l = 0; l < unlockedBuildings.length; l++) {
-            buildings[unlockedBuildings[l]].displayable = 1;
-            }
-        }
-
-        //Updates UI for new buildings
-        displayBuildings();
-
-        //Runs the corresponding function specific to this building
-        buildings[input.number].buildFunction();
-    }
-}
-
-
-function showCampfire(){
-    showTooltip(campfire);
-};
-
-function showShamanHut(){
-    showTooltip(shaman_hut);
-};
-
-function showHunterPost(){
-    showTooltip(hunter_post);
-};
-
-function showVillageCenter(){
-    showTooltip(village_center);
-};
-function showWoodChopper(){
-    showTooltip(woodchopper_post);
-};
-function showMinerHut(){
-    showTooltip(miner_hut);
-};
-function showWheatFarm(){
-    showTooltip(wheat_farm);
-}
-
-
-
-
-function buildCampfire(){
-    storage.food =+ 10;
-    storage.wood =+ 10;
-    storage.stone =+ 10;
-    
-};
-
-function buildShamanHut(){
-    show('#research-title',100);
-    hide('#build-button', 100);
-};
-
-function buildHunterPost(){
-};
-
-function buildVillageCenter(){
-    show('#manage-tribe-btn',100);
-    document.getElementById('manage-tribe-btn').addEventListener("click", manageTribe);
-};
-
-function buildWoodChopper(){
-};
-
-function buildMinerHut(){
-};
-
-function buildWheatFarm(){
-};
-
-//Auto-resource workers purchasing
-/*function hireForager() {
-    var workerCost = Math.floor(10 * Math.pow(1.1, foragers));
-    if (resource.food >= workerCost) {
-        foragers = foragers + 1;
-        resource.food = resource.food - workerCost;
-        document.getElementById('foragers').innerHTML = foragers;
-        document.getElementById('food').innerHTML = resource.food;
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1, foragers));
-    document.getElementById('foragerCost').innerHTML = nextCost;
-};
-
-function hireLumberjack() {
-    var workerCost = Math.floor(10 * Math.pow(1.1, lumberjacks));          //works out the cost of this lumberjack
-    if (resource.wood >= workerCost) {                                              //checks that the player can afford the lumberjack
-        lumberjacks = lumberjacks + 1;                                     //increases number of lumberjacks
-        resource.wood = resource.wood - workerCost;                                          //removes the wood spent
-        document.getElementById('lumberjacks').innerHTML = lumberjacks;    //updates the number of lumberjacks for the user
-        document.getElementById('wood').innerHTML = resource.wood;                  //updates the number of wood for the user
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1, lumberjacks));            //works out the cost of the next lumberjack
-    document.getElementById('lumberjackCost').innerHTML = nextCost;        //updates the lumberjack cost for the user
-};
-
-function hireMiner() {
-    var workerCost = Math.floor(10 * Math.pow(1.1, miners));
-    if (resource.stone >= workerCost) {
-        miners = miners + 1;
-        resource.stone = resource.stone - workerCost;
-        document.getElementById('miners').innerHTML = miners;
-        document.getElementById('stone').innerHTML = resource.stone;
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1, miners));
-    document.getElementById('minerCost').innerHTML = nextCost;
-};*/
--- js/research.js
+++ js/research.js
@@ -1,60 +1,3 @@
-//Research dictionary
-var research = [
-    stoneWorking = {
-        name: "Stone Working",
-        number: 0,
-        description: "Primitive tools used for everyday tasks.",
-        costs_1: "Stone",
-        costs_1_amount: 10,
-        costs_2: undefined,
-        costs_2_amount: undefined,
-        costs_3: undefined,
-        costs_3_amount: undefined,
-        costs_4: undefined,
-        costs_4_amount: undefined,
-        costs_5: undefined,
-        costs_5_amount: undefined,
-        effects_1: "Unlocks Stone Tools",
-        effects_2: "",
-        effects_3: "",
-        effects_4: "",
-        effects_5: "",
-        displayable: 1,
-        researched: 0,
-        researchFunction: researchStoneWorking,
-        showFunction: showStoneWorking,
-        message: "By carving stone into tools, you manage to improve the speed at which you perform tasks.",
-        unlocks_research: [],
-        unlocks_buildings: []
-    },
-    primitiveConstruction = {
-        name: "Primitive Construction",
-        number: 1,
-        description: "A roof over your people's heads.",
-        costs_1: "Stone",
-        costs_1_amount: 10,
-        costs_2: undefined,
-        costs_2_amount: undefined,
-        costs_3: undefined,
-        costs_3_amount: undefined,
-        costs_4: undefined,
-        costs_4_amount: undefined,
-        costs_5: undefined,
-        costs_5_amount: undefined,
-        effects_1: "Placeholder",
-        effects_2: "",
-        effects_3: "",
-        effects_4: "",
-        effects_5: "",
-        displayable: 1,
-        researched: 0,
-        researchFunction: researchPrimitiveConstruction,
-        showFunction: showPrimitiveConstruction,
-        message: "Placeholder",
-        unlocks_research: [],
-        unlocks_buildings: [4, 5]
-    },
-]
 
 
 //This function is an identical copy of buildBuilding in buildings.js, except for a few variables specific to research
@@ -153,3 +96,62 @@ function showPrimitiveConstruction(){
     showTooltip(primitiveConstruction)
 
 }
+
+
+//Research dictionary
+var research = [
+    stoneWorking = {
+        name: "Stone Working",
+        number: 0,
+        description: "Primitive tools used for everyday tasks.",
+        costs_1: "Stone",
+        costs_1_amount: 10,
+        costs_2: undefined,
+        costs_2_amount: undefined,
+        costs_3: undefined,
+        costs_3_amount: undefined,
+        costs_4: undefined,
+        costs_4_amount: undefined,
+        costs_5: undefined,
+        costs_5_amount: undefined,
+        effects_1: "Unlocks Stone Tools",
+        effects_2: "",
+        effects_3: "",
+        effects_4: "",
+        effects_5: "",
+        displayable: 1,
+        researched: 0,
+        researchFunction: researchStoneWorking,
+        showFunction: showStoneWorking,
+        message: "By carving stone into tools, you manage to improve the speed at which you perform tasks.",
+        unlocks_research: [],
+        unlocks_buildings: []
+    },
+    primitiveConstruction = {
+        name: "Primitive Construction",
+        number: 1,
+        description: "A roof over your people's heads.",
+        costs_1: "Stone",
+        costs_1_amount: 10,
+        costs_2: undefined,
+        costs_2_amount: undefined,
+        costs_3: undefined,
+        costs_3_amount: undefined,
+        costs_4: undefined,
+        costs_4_amount: undefined,
+        costs_5: undefined,
+        costs_5_amount: undefined,
+        effects_1: "Placeholder",
+        effects_2: "",
+        effects_3: "",
+        effects_4: "",
+        effects_5: "",
+        displayable: 1,
+        researched: 0,
+        researchFunction: researchPrimitiveConstruction,
+        showFunction: showPrimitiveConstruction,
+        message: "Placeholder",
+        unlocks_research: [],
+        unlocks_buildings: [4, 5]
+    },
+]
